0f24694c74c2b689e1f0f3a8d207c27e15c7218e
Nizeyumukiza <jpnizeyumukiza@gmail.com>
2023-03-04 19:45:23 -0800

    passed all tests

--- sql/11.sql
+++ sql/11.sql
@@ -1,12 +1,11 @@
 /*
  * List the name of all actors who have appeared in a movie that has the 'Behind the Scenes' special_feature
  */
-select distinct(last_name || ' ' || first_name) as "Actor Name"
+select distinct(first_name || ' ' || last_name) as "Actor Name"
 from film
-left join film_actor using(film_id)
-left join actor using(actor_id)
-where film_actor.film_id is not null and actor.actor_id is not null
-and film.film_id in (
+join film_actor using(film_id)
+join actor using(actor_id)
+where film.film_id in (
     select a.film_id
     from (select film_id, unnest(special_features) as "features" from film) as a
     where a.features = 'Behind the Scenes'
