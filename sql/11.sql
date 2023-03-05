3d45312aa37491f564a1485953340a73118efa5e
Nizeyumukiza <jpnizeyumukiza@gmail.com>
2023-03-04 19:17:28 -0800

    working on 11.sql

--- sql/11.sql
+++ sql/11.sql
@@ -6,7 +6,7 @@ from film
 left join film_actor using(film_id)
 left join actor using(actor_id)
 where film_actor.film_id is not null and actor.actor_id is not null
-and film_id in (
+and film.film_id in (
     select a.film_id
     from (select film_id, unnest(special_features) as "features" from film) as a
     where a.features = 'Behind the Scenes'
